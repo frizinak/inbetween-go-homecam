a02bb2c5a2214eb8a7a495a99f73274192ef04ea
Kobe Lipkens <kobelipkens@gmail.com>
2019-08-29 09:06:43 +0200

    restructure server struct

--- server/server.go
+++ server/server.go
@@ -44,30 +44,32 @@ type QualityConfig struct {
 type Server struct {
 	l *log.Logger
 
-	addr     string
-	maxPeers int
+	sem sync.Mutex
 
-	pass   []byte
-	secret []byte
+	net struct {
+		addr     string
+		pass     []byte
+		maxPeers int
 
-	reinitCam   bool
-	device      string
-	cam         *webcam.Webcam
-	activeRes   int
-	resolutions []webcam.FrameSize
-	data        []byte
+		data []byte
+
+		clients int
+		peers   int
+		bytes   uint64
+		since   time.Time
+	}
+
+	cam struct {
+		reinit      bool
+		device      string
+		cam         *webcam.Webcam
+		activeRes   int
+		resolutions []webcam.FrameSize
+	}
 
 	frameCount uint64
-
-	fps      int
-	jpegOpts *jpeg.Options
-
-	sem        sync.Mutex
-	clients    int
-	peers      int
-	bytes      uint64
-	since      time.Time
-	throughput float64
+	fps        int
+	jpegOpts   *jpeg.Options
 
 	quality QualityConfig
 }
@@ -80,17 +82,20 @@ func New(
 	quality QualityConfig,
 	maxPeers int,
 ) *Server {
-	return &Server{
+	s := &Server{
 		l:        l,
-		addr:     addr,
-		pass:     pass,
-		device:   device,
 		fps:      quality.MaxFPS,
 		jpegOpts: &jpeg.Options{Quality: quality.MinJPEGQuality},
-		since:    time.Now(),
 		quality:  quality,
-		maxPeers: maxPeers,
 	}
+
+	s.cam.device = device
+	s.net.addr = addr
+	s.net.maxPeers = maxPeers
+	s.net.since = time.Now()
+	s.net.pass = pass
+
+	return s
 }
 
 func (s *Server) initCam() {
@@ -111,18 +116,18 @@ func (s *Server) initCam() {
 
 func (s *Server) tryInitCam() error {
 	var err error
-	if s.cam != nil {
-		if err = s.cam.Close(); err != nil {
+	if s.cam.cam != nil {
+		if err = s.cam.cam.Close(); err != nil {
 			return err
 		}
 	}
 
-	s.cam, err = webcam.Open(s.device)
+	s.cam.cam, err = webcam.Open(s.cam.device)
 	if err != nil {
 		return err
 	}
 
-	formats := s.cam.GetSupportedFormats()
+	formats := s.cam.cam.GetSupportedFormats()
 	// TODO
 	// get first one for now
 	var pix webcam.PixelFormat
@@ -131,42 +136,42 @@ func (s *Server) tryInitCam() error {
 		break
 	}
 
-	if s.resolutions == nil {
-		sizes := s.cam.GetSupportedFrameSizes(pix)
-		s.resolutions = make([]webcam.FrameSize, 0, len(sizes))
+	if s.cam.resolutions == nil {
+		sizes := s.cam.cam.GetSupportedFrameSizes(pix)
+		s.cam.resolutions = make([]webcam.FrameSize, 0, len(sizes))
 		for i := range sizes {
 			res := int(sizes[i].MaxWidth * sizes[i].MinHeight)
 			if res < s.quality.MinResolution || res > s.quality.MaxResolution {
 				continue
 			}
-			s.resolutions = append(s.resolutions, sizes[i])
+			s.cam.resolutions = append(s.cam.resolutions, sizes[i])
 		}
-		s.activeRes = 0
+		s.cam.activeRes = 0
 
-		if len(s.resolutions) == 0 {
+		if len(s.cam.resolutions) == 0 {
 			for i := range sizes {
 				s.l.Printf("%dx%d = %d", sizes[i].MaxWidth, sizes[i].MinHeight, sizes[i].MaxWidth*sizes[i].MinHeight)
 			}
 			return errors.New("No resolutions found, try adjusting the min/max requirments")
 		}
 
-		sort.Slice(s.resolutions, func(i, j int) bool {
-			a, b := s.resolutions[i], s.resolutions[j]
+		sort.Slice(s.cam.resolutions, func(i, j int) bool {
+			a, b := s.cam.resolutions[i], s.cam.resolutions[j]
 			return a.MaxWidth*a.MinHeight > b.MaxWidth*b.MinHeight
 		})
 	}
 
-	_, _, _, err = s.cam.SetImageFormat(
+	_, _, _, err = s.cam.cam.SetImageFormat(
 		pix,
-		s.resolutions[s.activeRes].MaxWidth,
-		s.resolutions[s.activeRes].MaxHeight,
+		s.cam.resolutions[s.cam.activeRes].MaxWidth,
+		s.cam.resolutions[s.cam.activeRes].MaxHeight,
 	)
 
 	if err != nil {
 		return err
 	}
 
-	return s.cam.StartStreaming()
+	return s.cam.cam.StartStreaming()
 }
 
 func (s *Server) connErr(err error) {
@@ -177,24 +182,24 @@ func (s *Server) connErr(err error) {
 
 func (s *Server) addBytes(bytes uint64) {
 	s.sem.Lock()
-	s.bytes += bytes
-	since := time.Since(s.since).Seconds()
+	s.net.bytes += bytes
+	since := time.Since(s.net.since).Seconds()
 	if since > 3 {
-		s.throughput = float64(s.bytes) / since
-		s.since = time.Now()
-		s.bytes = 0
+		throughput := float64(s.net.bytes) / since
+		s.net.since = time.Now()
+		s.net.bytes = 0
 
 		oFPS := s.fps
 		oQuality := s.jpegOpts.Quality
-		oActiveRes := s.activeRes
+		oActiveRes := s.cam.activeRes
 
 		desired := s.quality.DesiredTotalThroughput
-		desiredClient := float64(s.clients) * s.quality.DesiredClientThroughput
+		desiredClient := float64(s.net.clients) * s.quality.DesiredClientThroughput
 		if desiredClient < desired {
 			desired = desiredClient
 		}
 
-		factor := s.throughput / desired
+		factor := throughput / desired
 		if factor < 0.01 {
 			factor = 0.01
 		} else if factor > 5 {
@@ -202,9 +207,9 @@ func (s *Server) addBytes(bytes uint64) {
 		}
 
 		if factor > 1.05 && s.fps == s.quality.MinFPS && s.jpegOpts.Quality == s.quality.MinJPEGQuality {
-			s.activeRes++
+			s.cam.activeRes++
 		} else if factor < 0.9 && s.fps == s.quality.MaxFPS && s.jpegOpts.Quality == s.quality.MaxJPEGQuality {
-			s.activeRes--
+			s.cam.activeRes--
 		} else if factor > 1.05 {
 			if s.fps <= s.quality.MinFPS+(s.quality.MaxFPS-s.quality.MinFPS)/2 {
 				s.jpegOpts.Quality = int(float64(s.jpegOpts.Quality) / factor)
@@ -232,24 +237,24 @@ func (s *Server) addBytes(bytes uint64) {
 			s.fps = s.quality.MaxFPS
 		}
 
-		if s.activeRes < 0 {
-			s.activeRes = 0
-		} else if s.activeRes >= len(s.resolutions) {
-			s.activeRes = len(s.resolutions) - 1
+		if s.cam.activeRes < 0 {
+			s.cam.activeRes = 0
+		} else if s.cam.activeRes >= len(s.cam.resolutions) {
+			s.cam.activeRes = len(s.cam.resolutions) - 1
 		}
 
-		if oActiveRes != s.activeRes {
-			s.reinitCam = true
+		if oActiveRes != s.cam.activeRes {
+			s.cam.reinit = true
 		}
 
 		if s.fps != oFPS ||
 			s.jpegOpts.Quality != oQuality ||
-			oActiveRes != s.activeRes {
+			oActiveRes != s.cam.activeRes {
 			s.l.Printf(
 				"%.1fkB/s throughput => Quality adjustment: %dx%d @ %dfps (jpeg: %d)",
-				s.throughput/1024,
-				s.resolutions[s.activeRes].MaxWidth,
-				s.resolutions[s.activeRes].MinHeight,
+				throughput/1024,
+				s.cam.resolutions[s.cam.activeRes].MaxWidth,
+				s.cam.resolutions[s.cam.activeRes].MinHeight,
 				s.fps,
 				s.jpegOpts.Quality,
 			)
@@ -260,18 +265,18 @@ func (s *Server) addBytes(bytes uint64) {
 
 func (s *Server) addClient(amount int) {
 	s.sem.Lock()
-	s.clients += amount
+	s.net.clients += amount
 	s.sem.Unlock()
 }
 
 func (s *Server) addPeer(amount int) error {
 	s.sem.Lock()
-	if s.peers+amount > s.maxPeers {
+	if s.net.peers+amount > s.net.maxPeers {
 		s.sem.Unlock()
-		return fmt.Errorf("Max number of peers reached (%d)", s.maxPeers)
+		return fmt.Errorf("Max number of peers reached (%d)", s.net.maxPeers)
 	}
 
-	s.peers += amount
+	s.net.peers += amount
 	s.sem.Unlock()
 	return nil
 }
@@ -330,7 +335,7 @@ func (s *Server) conn(c net.Conn) {
 	defer s.addClient(-1)
 	s.l.Println("New client", c.RemoteAddr())
 
-	pass := append(handshakeHash, s.pass...)
+	pass := append(handshakeHash, s.net.pass...)
 	crypter, err := crypto.NewImmutableKeyEncrypter(pass, 60, EncryptCost)
 	if err != nil {
 		s.connErr(err)
@@ -367,7 +372,7 @@ func (s *Server) conn(c net.Conn) {
 
 		frame = s.frameCount
 		w := &countWriter{n: 0, w: c}
-		err = crypter.Encrypt(bytes.NewBuffer(s.data), w)
+		err = crypter.Encrypt(bytes.NewBuffer(s.net.data), w)
 		if err != nil {
 			s.connErr(err)
 			return
@@ -382,7 +387,7 @@ func (s *Server) conn(c net.Conn) {
 }
 
 func (s *Server) Listen(output <-chan *bytes.Buffer) error {
-	ln, err := net.Listen("tcp", s.addr)
+	ln, err := net.Listen("tcp", s.net.addr)
 	if err != nil {
 		return err
 	}
@@ -401,10 +406,10 @@ func (s *Server) Listen(output <-chan *bytes.Buffer) error {
 					continue
 				}
 			}
-			s.data = d.Bytes()
+			s.net.data = d.Bytes()
 			s.frameCount++
 			if s.frameCount > 1e16 {
-				s.frameCount += 0
+				s.frameCount = 0
 			}
 		}
 	}()
@@ -426,33 +431,33 @@ func (s *Server) Start() (<-chan *bytes.Buffer, <-chan error) {
 	output := make(chan *bytes.Buffer, 1)
 	var last time.Time
 	go func() {
-		s.reinitCam = true
+		s.cam.reinit = true
 		for {
-			if s.reinitCam {
-				s.reinitCam = false
+			if s.cam.reinit {
+				s.cam.reinit = false
 				s.initCam()
 			}
 
-			err := s.cam.WaitForFrame(1)
+			err := s.cam.cam.WaitForFrame(1)
 			switch err.(type) {
 			case nil:
 			case *webcam.Timeout:
 				continue
 			default:
 				s.l.Printf("Failed waiting for cam frame: %s", err)
-				s.reinitCam = true
+				s.cam.reinit = true
 				continue
 			}
 
 			if time.Since(last) < time.Second/time.Duration(s.fps) {
-				s.cam.ReadFrame()
+				s.cam.cam.ReadFrame()
 				continue
 			}
 
-			_d, err := s.cam.ReadFrame()
+			_d, err := s.cam.cam.ReadFrame()
 			if err != nil {
 				s.l.Printf("Failed reading cam frame: %s", err)
-				s.reinitCam = true
+				s.cam.reinit = true
 				continue
 			}
 			d := make([]byte, len(_d))
